0ff5181adfee356f040bb333d025e3d618a7e842
ruby <ruby@rubykobayashi.com>
2018-12-19 15:34:03 +0000

    8 Q9 & 10

--- directory.rb
+++ directory.rb
@@ -4,44 +4,48 @@ def input_students
   puts "If you make a typo, type typo, correct the spelling and press enter"
   while true do
   puts "Please enter a name of the student or to finish just hit enter twice"
-  name = gets.chomp
+  name = gets.strip
   p name
   if name.empty?
     break
 
   elsif students.empty? && name == "typo"
     p "please re-enter the name of the student"
-    name = gets.chomp
+    name = gets.strip
 
   elsif !students.empty? && name == "typo"
    puts "please re-enter the cohort"
    cohort = gets.chomp
    students.last[:cohort] = cohort
-   if cohort.empty?
-   students << {name: name, cohort: :unknown}
    puts "Please enter a name of the student or to finish just hit enter twice"
-   name = gets.chomp
+   name = gets.strip
    if name.empty?
      break
 end
 end
-end
     puts "Please enter the cohort of the student"
-    cohort = gets.chomp
+    cohort = gets.strip
 
     if cohort == "typo"
       puts "please re-enter the name"
-      name = gets.chomp
+      name = gets.strip
       puts "Please enter the cohort of the student"
-      cohort = gets.chomp
+      cohort = gets.strip
       students << {name: name, cohort: cohort}
+      if students.count == 1
+        puts "Now we have 1 student"
+      else
       puts "Now we have #{students.count} students"
+    end
     elsif cohort.empty?
       students << {name: name, cohort: :unknown}
       else
     students << {name: name, cohort: cohort}
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
     puts "Now we have #{students.count} students"
-
+  end
 
 end
 end
@@ -57,14 +61,21 @@ puts "-------------"
 end
 
 def print(students)
-    students.each do |student|
-    puts "#{student[:name]}, (#{student[:cohort]} cohort)".center(80, "-")
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 
+
+
+
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  elsif names.count > 1
+    puts "Overall, we have #{names.count} great students"
+end
 end
 
 students = input_students
