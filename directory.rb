271dfa82ebee4721824c95c601ccb84007198f19
ruby <ruby@rubykobayashi.com>
2018-12-19 17:35:53 +0000

    13: added load_students method

--- directory.rb
+++ directory.rb
@@ -1,52 +1,97 @@
+@students = []
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets .chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
   # get another name from the user
   name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
-def print_header(students)
-  if students.empty?
-    nil
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+# iterate over the array of students
+@students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+end
+file.close
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+print_header
+print_student_list
+print_footer
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+  @students << {name: name, cohort: cohort.to_sym }
+end
+file.close
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
   else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
-end
 
-def print(students)
-  if students.empty?
-    puts "You have not entered any students into the directory"
-  else
-  students.each do |student|
+
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
  end
 end
+
+
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 
-def print_footer(students)
-  if students.empty?
-  nil
-else
-  puts "Overall, we have #{students.count} great students"
-end
-end
-
-
-students = input_students
-print_header(students)
-print(students)
-print_footer(students)
+interactive_menu
