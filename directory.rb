fe5ac7d1a16e5cb6c6515a9b96f9f09a568f33b4
ruby <ruby@rubykobayashi.com>
2018-12-20 12:19:04 +0000

    Step 14 Q1

--- directory.rb
+++ directory.rb
@@ -1,40 +1,5 @@
 @students = []
 
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets .chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-  # add the student hash to the array
-  @students << {name: name, cohort: :november}
-  puts "Now we have #{@students.count} students"
-  # get another name from the user
-  name = gets.chomp
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-end
-end
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-# iterate over the array of students
-@students.each do |student|
-  student_data = [student[:name], student[:cohort]]
-  csv_line = student_data.join(",")
-  file.puts csv_line
-end
-file.close
-end
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -43,19 +8,11 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-print_header
-print_student_list
-print_footer
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
 end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-  @students << {name: name, cohort: cohort.to_sym }
-end
-file.close
 end
 
 def process(selection)
@@ -75,11 +32,37 @@ def process(selection)
   end
 end
 
-def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  @name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !@name.empty? do
+  # add the student hash to the array
+
+  add_students_info_to_list
+  puts "Now we have #{@students.count} students"
+  # get another name from the user
+  @name = STDIN.gets.chomp
+end
+  end
+
+def add_students_info_to_list
+  student_hash_info = {name: @name, cohort: :november}
+  @students << student_hash_info
 end
 
+def show_students
+print_header
+print_student_list
+print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
 def print_student_list
   @students.each do |student|
@@ -87,11 +70,44 @@ def print_student_list
  end
 end
 
-
-
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
 
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+# iterate over the array of students
+@students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+end
+file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open("students.csv" , "r")
+  file.readlines.each do |line|
+  @name, cohort = line.chomp.split(',')
+  add_students_info_to_list
+end
+file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
