30760c008fb96ebcce33c89f0cba3e92b9ae990b
ruby <ruby@rubykobayashi.com>
2018-12-24 20:42:24 +0000

    8 Q8

--- directory.rb
+++ directory.rb
@@ -1,115 +1,85 @@
-@students = []
-
-def print_menu
-  puts "1. Input the students
-        2. Show the students
-        3. Save the list to students.csv
-        4. Load the list from students.csv
-        9. Exit"
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-end
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you mean, try again"
-  end
-end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # while the name is not empty, repeat this code
-  while !@name.empty? do
-    # get the first name
-  @name = STDIN.gets.chomp
-  # add the student hash to the array
-  add_students_info_to_list
-  puts "Now we have #{@students.count} students"
-  # get another name from the user
+  students = []
+  puts "If you make a typo, type typo, correct the spelling and press enter"
+  while true do
+  puts "Please enter a name of the student or to finish just hit enter twice"
+  name = gets.chomp
+  p name
+  if name.empty?
+    break
+
+  elsif students.empty? && name == "typo"
+    p "please re-enter the name of the student"
+    name = gets.chomp
+
+  elsif !students.empty? && name == "typo"
+   puts "please re-enter the cohort"
+   cohort = gets.chomp
+   students.last[:cohort] = cohort
+   puts "Please enter a name of the student or to finish just hit enter twice"
+   name = gets.chomp
+   if name.empty?
+     break
+end
+end
+    puts "Please enter the cohort of the student"
+    cohort = gets.chomp
+
+    if cohort == "typo"
+      puts "please re-enter the name"
+      name = gets.chomp
+      puts "Please enter the cohort of the student"
+      cohort = gets.chomp
+      students << {name: name, cohort: cohort}
+      puts "Now we have #{students.count} students"
+    elsif cohort.empty?
+      students << {name: name, cohort: :unknown}
+      else
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
+
 
 end
-  end
-
-def add_students_info_to_list
-  student_hash_info = {name: @name, cohort: :november}
-  @students << student_hash_info
+end
+  students
 end
 
-def show_students
-print_header
-print_student_list
-print_footer
-end
+
+
 
 def print_header
-  puts "The students of Villains Academy
-  -------------"
-end
-
-def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
- end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
+puts "The students of Villains Academy"
+puts "-------------"
 end
 
 
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-# iterate over the array of students
-@students.each do |student|
-  student_data = [student[:name], student[:cohort]]
-  csv_line = student_data.join(",")
-  file.puts csv_line
-end
-file.close
-end
-
-def load_students(filename = "students.csv")
-  file = File.open("students.csv" , "r")
-  file.readlines.each do |line|
-  @name, cohort = line.chomp.split(',')
-  add_students_info_to_list
-end
-file.close
-end
-
-def try_load_students(filename = "students.csv")
-  filename = ARGV.first
-  if filename.nil?
-    filename = "students.csv"
-    load_students
-       puts "Loaded #{@students.count} from #{filename}"
-     elsif File.exists?(filename)
-      load_students
-       puts "Loaded #{@students.count} from #{filename}"
-     else
-       puts "Sorry #{filename} doesn't exist"
-       exit
+def print_cohort(students)
+  cohort_month = []
+  students.map { |student| cohort_month << student[:cohort] }
+  cohort_groups = cohort_month.uniq
+  cohort_index = 0
+  while cohort_groups.length > cohort_index do
+    puts "#{cohort_groups[cohort_index].capitalize} cohort members:".center(50)
+    students.each do |student|
+      if student[:cohort] == cohort_groups[cohort_index]
+        puts student[:name].center(50)
+    end
+    end
+  cohort_index += 1
   end
 end
 
-try_load_students
-interactive_menu
+def print_footer(names)
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  elsif names.count > 1
+    puts "Overall, we have #{names.count} great students"
+end
+end
+
+
+students = input_students
+  print(students)
+print_footer(students)
+print_cohort(students)
